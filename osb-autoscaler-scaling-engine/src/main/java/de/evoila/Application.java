1a658f310caf1ddfd7ab1268f440bcb7c0206512
patrick weber <pweber@evoila.de>
2018-09-21 21:42:40 +0200

    chore(update) update spring boot

--- osb-autoscaler-scaling-engine/src/main/java/de/evoila/Application.java
+++ osb-autoscaler-scaling-engine/src/main/java/de/evoila/Application.java
@@ -1,22 +1,26 @@
 package de.evoila;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.util.Assert;
 
 import de.evoila.cf.autoscaler.engine.model.CfBean;
 import de.evoila.cf.autoscaler.engine.model.EnginePlatform;
 import de.evoila.cf.autoscaler.engine.model.ScalerBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.util.Assert;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableConfigurationProperties({CfBean.class, EnginePlatform.class, ScalerBean.class})
 public class Application {
 
 	public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        ApplicationContext ctx = springApplication.run(args);
+
         Assert.notNull(ctx, "Could not properly start the application.");
 	}
 }
